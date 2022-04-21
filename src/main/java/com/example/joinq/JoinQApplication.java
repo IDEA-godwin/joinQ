17d4e3b5d40b84ed5f8c2b2e155e1b0ba7989937
Godwin Igwurube <godwinaquinas@gmail.com>
2022-04-21 14:25:57 +0100

    initial commit

---
+++ src/main/java/com/example/joinq/JoinQApplication.java
@@ -0,0 +1,13 @@
+package com.example.joinq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoinQApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JoinQApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/joinq/JoinQApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.joinq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoinQApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
